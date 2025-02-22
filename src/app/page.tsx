ce9dc6d6dd91e55f1fe7a9c8068e525e8370f516
sar9484 <batbayarsaruul0@gmail.com>
2025-02-22 16:45:16 +0800

    asdf

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,17 @@
-'use client'
+"use client";
 import { useEffect } from "react";
 import axios from "axios";
 export default function Home() {
   const getData = async () => {
     try {
-      const response = await axios.get('http://localhost:5000');
+      const response = await axios.get("http://localhost:5000/login");
       console.log(response);
     } catch (error) {
-      console.error('Error fetching data:', error);
+      console.error("Error fetching data:", error);
     }
   };
-useEffect(()=>{
-  getData()
-},[])
-  return (
-    <div>
-    </div>
-  );
+  useEffect(() => {
+    getData();
+  }, []);
+  return <div></div>;
 }
