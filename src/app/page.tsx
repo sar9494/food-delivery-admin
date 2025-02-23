4da73ce711eaf1f0836d93c30d5ff306f90fd1ba
sar9484 <batbayarsaruul0@gmail.com>
2025-02-23 23:32:27 +0800

    fdadf

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,18 @@
 "use client";
 import { useEffect } from "react";
+import { LogIn } from "../components/LogIn";
 import axios from "axios";
 export default function Home() {
   const getData = async () => {
     try {
-      const response = await axios.get("http://localhost:5000/login");
+      const response = await axios.get("http://localhost:5000");
       console.log(response);
     } catch (error) {
       console.error("Error fetching data:", error);
     }
   };
   useEffect(() => {
-    getData();
+    // getData();
   }, []);
   return <div></div>;
 }
