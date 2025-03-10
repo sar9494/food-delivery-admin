4a9fce16b0a14a3204f419aeea9474ccd1fe5625
sar9484 <batbayarsaruul0@gmail.com>
2025-03-10 11:25:27 +0800

    asdf

--- src/app/(orders)/_components/TableHeader.tsx
+++ src/app/(orders)/_components/TableHeader.tsx
@@ -1,13 +1,21 @@
 import { Checkbox } from "@/components/ui/checkbox";
 export const TableHeader = () => {
   return (
-    <div className="flex items-center space-x-2">
+    <div className="flex items-center space-x-2 w-full">
       <Checkbox id="terms" />
       <label
         htmlFor="terms"
-        className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+        className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 w-full"
       >
-        Accept terms and conditions
+        <div className=" flex justify-around w-full bg-gray-300">
+          <p>№</p>
+          <p>Customer</p>
+          <p>Food</p>
+          <button></button>
+          <p>Total</p>
+          <p>Delivery address</p>
+          <p>Delivery state</p>
+        </div>
       </label>
     </div>
   );
